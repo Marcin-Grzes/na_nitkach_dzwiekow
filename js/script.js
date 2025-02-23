eebc2bd8ae1d0406da1145a408e6437d159009f5
Marcin-Grzes <marcin@grzes.dev>
2025-02-23 16:46:39 +0100

    Init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const navMobile = document.querySelector('.nav__items-mobile');
+const navBTN = document.querySelector('.hamburger');
+const allNavItems = document.querySelectorAll('.nav__item--mobile');
+
+const handleNavMobile = () => {
+    navBTN.classList.toggle('is-active');
+    navMobile.classList.toggle('nav__items-mobile--active');
+
+    handleNavAnimation()
+}
+
+const handleNavAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach((item) => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+
+}
+
+allNavItems.forEach(item => {
+    item.addEventListener('click', handleNavMobile);
+})
+
+navBTN.addEventListener('click', handleNavMobile);
